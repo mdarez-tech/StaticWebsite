3e8ab41a16195576724b814738372310d37634c9
mdarez-tech <m.darez@it.misuratau.edu.ly>
2025-11-12 23:51:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("showInfo").addEventListener("click");
+document.getElementById("infoText").textContent = "تستخدم الحوسبة السحابية في مجالات مثل الدكاء الاصطناعي والتخزين و استضافة المواقع";
